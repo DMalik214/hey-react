0dcb96027a0e68422488a0b27ac8b177da76f755
Deepak Malik <163868959+DMalik214@users.noreply.github.com>
2024-06-10 22:01:40 +0530

    Update DigitalBanking.js

--- src/components/DigitalBanking.js
+++ src/components/DigitalBanking.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import bankImg from '.\bankImg.png';
 
 const DigitalSection = styled.section`
   background: #fff;
@@ -19,9 +20,17 @@ const DigitalDescription = styled.p`
   margin: 0 auto;
 `;
 
+const FeatureImage = styled.img`
+  max-height: 80px;
+  margin-bottom: ${(props) => (props.sideways ? '0' : '20px')};
+  margin-right: ${(props) => (props.sideways ? '20px' : '0')};
+`;
+
+
 const DigitalBanking = () => {
   return (
     <DigitalSection>
+      <FeatureImage src={bankImg} alt="Digital Banking" sideways />
       <DigitalTitle>Go Digital Now!</DigitalTitle>
       <DigitalDescription>Our digital platform allows you to access your money from anywhere, anytime. With our innovative security features and first-class service, we combine the best of traditional banking with modern convenience.</DigitalDescription>
       <button>Get Started</button>
